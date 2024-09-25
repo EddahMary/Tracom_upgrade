134b7458d971c56e9169cfc1655b388c6713e6d0
Johnson <johnsonnjichi@gmail.com>
2024-09-26 02:54:58 +0300

    feat: imports

--- tracom-app/src/components/About.js
+++ tracom-app/src/components/About.js
@@ -2,6 +2,7 @@ const About = () => {
     return (
         <div className="about">
             <h1>About Us</h1>
+            <p>We are here because of you.</p>
         </div>
     )
 }
--- tracom-app/src/components/Team.js
+++ tracom-app/src/components/Team.js
@@ -1,3 +1,6 @@
+import React from "react";
+
+
 const Team = () => {
     return(
         <div className="team">
